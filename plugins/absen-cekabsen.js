d5c63d1d5ea91d100becdc75f03a017981d779ce
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-03 12:46:07 +0530

    Update absen-cekabsen.js

--- plugins/absen-cekabsen.js
+++ plugins/absen-cekabsen.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
     if (!(id in conn.absen)) {
-        await conn.sendButton(m.chat, `Tidak ada absen berlangsung digrup ini!\n\nketik *${usedPrefix}mulaiabsen* untuk memulai absen`.trim(), '© stikerin', 'Mulai', `${usedPrefix}mulaiabsen`, m)
+        await conn.sendButton(m.chat, `No absenteeism takes place!`, '© MilfBOT', 'Start', `${usedPrefix}startabsent`, m)
         throw false
     }
 
     let d = new Date
-    let date = d.toLocaleDateString('id', {
+    let date = d.toLocaleDateString('en-US', {
         day: 'numeric',
         month: 'long',
         year: 'numeric'
@@ -15,19 +15,18 @@ let handler = async (m, { conn, usedPrefix }) => {
     let absen = conn.absen[id][1]
     let list = absen.map((v, i) => `├ ${i + 1}. @${v.split`@`[0]}`).join('\n')
     let caption = `
-Tanggal: ${date}
+Date: ${date}
 ${conn.absen[id][2]}
-    
-┌〔 daftar absen 〕
-│ 
+┌〔 Absent list 〕
+│
 ├ Total: ${absen.length}
 ${list}
-│ 
+│
 └────`.trim()
-    await conn.send2Button(m.chat, caption, '© stikerin', 'Absen', `${usedPrefix}absen`, 'Hapus', `${usedPrefix}hapusabsen`, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } })
+    await conn.send2Button(m.chat, caption, '© MilfBOT', 'Absent', `${usedPrefix}absent`, 'delete', `${usedPrefix}removeabsent`, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } })
 }
-handler.help = ['cekabsen']
+handler.help = ['checkabsent']
 handler.tags = ['absen']
-handler.command = /^cekabsen$/i
-handler.group = true
+handler.command = /^checkabsent$/i
+
 module.exports = handler
