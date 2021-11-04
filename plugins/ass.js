587f0ba09b73fb54eca54941bf02121bd146c950
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-04 17:09:30 +0530

    Create ass.js

---
+++ plugins/ass.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn }) => {
+  if (!db.data.settings.nsfw) throw "NSFW mode is *OFF*";
+ 
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/ass?APIKEY=MahliKey', '', 'sange~an', m)
+  
+}
+handler.help = ['ass']
+handler.tags = ['fun']
+handler.command = /^(ass)$/i
+
+handler.limit = true
+handler.group = true
+
+module.exports = handler
