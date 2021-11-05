1fb13c1a8a35e4b7831ea0146331cddb4d64fd75
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-06 01:25:20 +0530

    Update quotemaker.js

--- plugins/quotemaker.js
+++ plugins/quotemaker.js
@@ -1,11 +1,17 @@
-let handler = async (m, { conn, text }) => {
-  let [teks, wm] = text.split`|`
-  await conn.sendFile(m.chat, global.API('xteam', '/quotemaker', { text: teks, wm: wm ? wm : conn.getName(m.sender) }, 'APIKEY'), 'file.jpg', 'Nih', m, 0, { thumbnail: Buffer.alloc(0) })
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command}) => {
+    let [q, n] = text.split `|`
+    if (!q) throw `Type ${usedPrefix + command} <teks | author>`
+    let user = global.db.data.users[m.sender].name
+    let name = n ? n : user ? user : await conn.getName(m.sender)
+    let res = await fetch(`https://terhambar.com/aw/qts/?kata=${q}&author=${name}&tipe=random`)
+    if (res.status !==200) throw 'Server error'
+    let json = await res.json()
+    await conn.sendFile(m.chat, json.result, 'q.jpg', '©DONE', m)
 }
-handler.help = ['quote'].map(v => v + 'maker <teks>|<wm>')
+handler.help = ['quotemaker2 <text | wm>']
 handler.tags = ['nulis']
-handler.command = /^quotemaker$/i
-
+handler.command = /^q(uote)?maker?2$/i
 handler.limit = true
-
+//MadeByLeviBot
 module.exports = handler
