3352ab88288c3c26ddeaec51f03fbf19875634e0
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-06 01:02:08 +0530

    Update kbbi.js

--- plugins/kbbi.js
+++ plugins/kbbi.js
@@ -1,17 +1,17 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text, command, usedPrefix }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} membaca`
-    let res = await fetch(global.API('pencarikode', '/kbbi', { kata: text }, 'APIKEY'))
-    if (!res.ok) throw eror
+let handler = async (m, { text }) => {
+    if (!text) throw `Uhm.. where's the text?`
+    let res = await fetch(global.API('xteam', '/kbbi', { kata: text }, 'APIKEY'))
+    if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
-    let list = json.kata.list.map((v, i) => `${i + 1}. ${v}`).join('\n')
-    m.reply(`${json.kata.word}
+    let list = json.message.list.map((v, i) => `${i + 1}. ${v}`).join('\n')
+    m.reply(`${json.message.word}
     
 ${list}
 `.trim())
 }
-handler.help = ['kbbi <teks>']
+handler.help = ['kbbi <text>']
 handler.tags = ['internet']
 handler.command = /^kbbi$/i
 module.exports = handler
