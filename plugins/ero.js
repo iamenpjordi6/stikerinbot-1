f6465cacb30022cc44597e93812e266d41f3b895
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-06 08:41:14 +0530

    Create ero.js

---
+++ plugins/ero.js
@@ -0,0 +1,14 @@
+let hmtai = require('hmtai')
+
+let handler = async(m, { conn }) => {
+  if (!db.data.settings.nsfw) throw "NSFW mode is *OFF*";
+  let img = await hmtai.nsfw.ero()
+  await conn.sendFile(m.chat, img, '', '© MilfBOT', m)
+
+}
+handler.help = ['ero']
+handler.tags = ['anime']
+
+handler.command = /^(ero)$/i
+
+module.exports = handler
