60492f0b087922532336c5e4ac0c3e8974d75963
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-06 17:22:44 +0530

    Create awoo.js

---
+++ plugins/awoo.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn }) => {
+  m.reply('Loading...')
+  let res = `https://velgrynd.herokuapp.com/api/image/awoo`
+  conn.sendFile(m.chat, res, 'milf.jpg', '© MilfBOT', m)
+}
+handler.help = ['awoo']
+handler.tags = ['anime']
+handler.command = /^(awoo)$/i
+module.exports = handler
