c67c6dbfb64c2b643d86aa2c5d9ee85f899cc6b9
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-06 08:26:48 +0530

    Update milf.js

--- plugins/milf.js
+++ plugins/milf.js
@@ -1,4 +1,5 @@
 let handler = async (m, { conn }) => {
+  if (!db.data.settings.nsfw) throw "NSFW mode is *OFF*";
   m.reply('Loading...')
   let res = `https://velgrynd.herokuapp.com/api/image/milf`
   conn.sendFile(m.chat, res, 'milf.jpg', '© MilfBOT', m)
