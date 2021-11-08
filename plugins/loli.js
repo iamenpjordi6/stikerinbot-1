8dbf3e49ac771d2bdfa747ce69f5adf3ffaa1126
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-08 12:06:41 +0530

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -8,6 +8,7 @@ let url = json[Math.floor(Math.random() * json.length)]
 await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Random Loli', '© MilfBOT', 'Get Again', '/loli', m)
 }
 handler.command = /^(loli)$/i
+handler.group = true
 handler.tags = ['anime']
 handler.help = ['fun']
 module.exports = handler
