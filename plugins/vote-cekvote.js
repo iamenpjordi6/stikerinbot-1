608c3cbbd956508dea40d3251ecc83ea6c476169
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-08 08:19:48 +0530

    Update vote-cekvote.js

--- plugins/vote-cekvote.js
+++ plugins/vote-cekvote.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
     if (!(id in conn.vote)) {
-        await conn.sendButton(m.chat, `_*No voting in this group!*_`, '© MilfBOT', 'START VOTE', `${usedPrefix}startvote`, m)
+        await conn.sendButton(m.chat, `_*tidak ada voting digrup ini!*_`, '© stikerin', 'MULAI VOTE', `${usedPrefix}mulaivote`, m)
         throw false
     }
 
@@ -11,22 +11,22 @@ let handler = async (m, { conn, usedPrefix }) => {
     let caption = `
     〔 VOTE 〕
 
-*Reason:* ${reason}
+*Alasan:* ${reason}
 
-*UPVOTES*
+*UPVOTE*
 _Total: ${upvote.length}_
 ${upvote.map(u => '@' + u.split('@')[0]).join('\n')}
 
-*DEVOTES*
+*DEVOTE*
 _Total: ${devote.length}_
 ${devote.map(u => '@' + u.split('@')[0]).join('\n')}
 
-_by Jordi_
+_by ariffb_
     `.trim()
-    await conn.send3Button(m.chat, caption, '© MilfBOT', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`, 'DELETE VOTE', `${usedPrefix}deletevote`, m, { contextInfo: { mentionedJid } })
+    await conn.send3Button(m.chat, caption, '© stikerin', 'UPVOTE', `${usedPrefix}upvote`, 'DEVOTE', `${usedPrefix}devote`, 'HAPUS VOTE', `${usedPrefix}hapusvote`, m, { contextInfo: { mentionedJid } })
 }
-handler.help = ['checkvote']
+handler.help = ['cekvote']
 handler.tags = ['vote']
-handler.command = /^checkvote$/i
+handler.command = /^cekvote$/i
 handler.group = true
 module.exports = handler
