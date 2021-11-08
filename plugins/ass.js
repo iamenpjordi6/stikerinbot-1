0612330c8450097c9b6c84929d4d5292e9ee612d
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-08 08:46:04 +0530

    Update ass.js

--- plugins/ass.js
+++ plugins/ass.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   if (!db.data.settings.nsfw) throw "NSFW mode is *OFF*";
  
-  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/ass?APIKEY=MahliKey', '', 'sange~an', m)
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/ass?APIKEY=MahliKey', '', 'MilfBOT', m)
   
 }
 handler.help = ['ass']
