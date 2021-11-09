04ffc74f67d9aeaae26de6cf3ab762b027cda3a0
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-10 01:11:02 +0530

    Create xnxxdl.js

---
+++ plugins/xnxxdl.js
@@ -0,0 +1,18 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, args }) => {
+ if (!args[0]) throw 'Uhm..Send mediafire download URL?'
+ let res = await fetch(API('Velgrynd', '/api/xnxxdl', { url: args[0] }))
+ if (!res.ok) throw await res.text()
+ let json = await res.json()
+ let { title, low } = json.result
+ m.reply(JSON.stringify(json.result, null, 2))
+ conn.sendFile(m.chat, low, title, '', m)
+}
+handler.help = ['xnxxdl'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^xnxxdl$/i
+
+handler.limit = true
+
+module.exports = handler
