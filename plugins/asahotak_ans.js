d0ef460d36ebcd689201a1214a4633932ca709aa
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-09 21:20:47 +0530

    Update asahotak_ans.js

--- plugins/asahotak_ans.js
+++ plugins/asahotak_ans.js
@@ -5,17 +5,17 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*ao/i.test(m.quoted.contentText)) return !0
     this.asahotak = this.asahotak ? this.asahotak : {}
-    if (!(id in this.asahotak)) return m.reply('The matter has ended')
+    if (!(id in this.asahotak)) return m.reply('The Riddle has ended')
     if (m.quoted.id == this.asahotak[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
-        if (['.ao', 'Bantuan', ''].includes(m.text)) return !0
+        if (['.ao', 'Hint', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.answer.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.asahotak[id][2]
-            await this.sendButton(m.chat, `*Right!* +${this.asahotak[id][2]} XP`, '© MilfBOT', 'Brain Teaser', '.asahotak', m)
+            await this.sendButton(m.chat, `*You Guessed Right!* \nHere is your reward +${this.asahotak[id][2]} XP`, '© MilfBOT', 'Brain Teaser', '.brainteaser', m)
             clearTimeout(this.asahotak[id][3])
             delete this.asahotak[id]
         } else if (similarity(m.text.toLowerCase(), json.answer.toLowerCase().trim()) >= threshold) m.reply(`*a little more!*`)
-        else m.reply(`*Wrong!*`)
+        else m.reply(`*Wrong Guess! Try again*`)
     }
     return !0
 }
