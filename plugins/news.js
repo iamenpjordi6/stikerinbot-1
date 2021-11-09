eab4de1f83fcc42b3e02ad5f9fe5183a4231a46a
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-09 08:38:40 +0530

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -1,18 +1,20 @@
 const axios = require('axios')
 let handler = async (m, { conn }) => {
    try{
+      const news_api='https://newsapi.org/v2/top-headlines?country=us&apiKey=ba3e0f8350064654a0ee6a89005a0922'
+    async function getNews(){
+        const response = await fetch(news_api);
+        const data = await response.json();
+       
+ 
+      conn.reply(m.chat, ``Title: ${data.articles[1].title} , 
+by: ${data.articles[1].author}
 
-        const response = axios.get(`https://api.zeks.me/api/foxnews?apikey=TJpDLutSDYzzlMF2OIa4zYeTfG4`)
-        const res = await response
-        const title = res.data["result"][0]["title"]["value"]
-        const url = res.data["result"][0]["url"]["value"]
-        const country = res.data["result"][0]["country"]["value"]
-        const time = res.data["result"][0]["time"]["value"]
-        const content = res.data["result"][0]["content"]["value"]
-        
-        conn.reply(m.chat,`
-        🌸Title: ${title} , ${Country} , ${time}\n ${content}\nRead Full Article: ${url}
-        `.trim(),m)
+Description: ${data.articles[1].description}` .trim(),m)
+    }catch(e){
+throw 'location not found' 
+console.log(e)
+
     }
 }
 
