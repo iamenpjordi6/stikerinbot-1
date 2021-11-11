729f9e4c8b25a0c3e63d32a51c344ec717522b84
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-11 10:28:59 +0530

    Create stickereffect2.js

---
+++ plugins/stickereffect2.js
@@ -0,0 +1,39 @@
+//made by Anshul
+const uploadImage = require('../lib/uploadImage')
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+const effects = ['calender', 'draw-image', 'sketch-image', 'burning-image']
+
+let handler = async (m, { conn, usedPrefix, text, command }) => {
+  let effect = text.trim().toLowerCase()
+  if (!effects.includes(effect)) throw `
+┌─〔 Effects List 〕
+${effects.map(effect => `├ ${effect}`).join('\n')}
+└────
+Example: 
+${usedPrefix + command} calender
+`.trim()
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Reply to the picture!'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} not supported`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let apiUrl = global.API('zeks', '/search/freefire',/', encodeURIComponent(effect), {
+    avatar: url
+  })
+  try {
+    let stiker = await sticker(null, apiUrl, global.packname, global.author)
+    await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+  } catch (e) {
+    await conn.sendFile(m.chat, apiUrl, 'image.png', null, m, 0, { thumbnail: await (await fetch(apiUrl)).buffer() })
+  }
+}
+
+handler.help = ['stickerfilter']
+handler.tags = ['sticker']
+handler.command = /^(s(tic?ker)?filter)$/i
+
+module.exports = handler
