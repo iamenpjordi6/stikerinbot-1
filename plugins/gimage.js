88e08cd61ac823c9c685ef43f39387f565edd381
iamenpjordi6 <90175854+iamenpjordi6@users.noreply.github.com>
2021-11-12 19:36:17 +0530

    Revert "Update and rename gimage.js to image.js"
    This reverts commit 8f1c53ebbd71daf56e7791081011ed1baf718ccf.

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
   if (!url) throw '404 Not Found'
   conn.sendFile(m.chat, url, 'gimage', '', m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
-handler.help = ['image <search>']
+handler.help = ['gimage <search>', 'image <search>']
 handler.tags = ['internet']
 handler.command = /^(g?image)$/i
 
